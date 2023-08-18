4c7b780cc4a2661b59a559803494f1c76948208d
Vipul <vipulupadhyay563@gmail.com>
2023-08-18 10:38:08 +0530

    Add ef core database opertaions with repository pattern

---
+++ BookStore.Data/Repository/BookRepository.cs
@@ -0,0 +1,69 @@
+﻿using BookStore.Data.Context;
+using BookStore.Domain.Interfaces;
+using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Data.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookstoreDbContext _dbContext;
+
+        public BookRepository(BookstoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Book>> GetAllBooksAsync()
+        {
+            return await _dbContext.Books.ToListAsync();
+        }
+
+        public async Task<Book> GetBookByIdAsync(int id)
+        {
+            return await _dbContext.Books.FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task AddBookAsync(Book book)
+        {
+            _dbContext.Books.Add(book);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateBookAsync(int id, Book updatedBook)
+        {
+            var existingBook = await _dbContext.Books.FindAsync(id);
+            if (existingBook != null)
+            {
+                existingBook.Title = updatedBook.Title;
+                existingBook.Author = updatedBook.Author;
+                existingBook.Genre = updatedBook.Genre;
+                existingBook.Price = updatedBook.Price;
+                await _dbContext.SaveChangesAsync();
+            }
+            else
+            {
+                throw new ArgumentException("Book not found.", nameof(id));
+            }
+        }
+
+        public async Task DeleteBookAsync(int id)
+        {
+            var book = await _dbContext.Books.FindAsync(id);
+            if (book != null)
+            {
+                _dbContext.Books.Remove(book);
+                await _dbContext.SaveChangesAsync();
+            }
+            else
+            {
+                throw new ArgumentException("Book not found.", nameof(id));
+            }
+        }
+    }
+}
--- BookStore/Controllers/BooksController.cs
+++ BookStore/Controllers/BooksController.cs
@@ -1,12 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Domain.Interfaces;
+using BookStore.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
 {
-    public class BooksController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
         {
-            return View();
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetAllBooks()
+        {
+            var books = await _bookRepository.GetAllBooksAsync();
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBookById(int id)
+        {
+            var book = await _bookRepository.GetBookByIdAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> AddBook(Book book)
+        {
+            await _bookRepository.AddBookAsync(book);
+            return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook(int id, Book updatedBook)
+        {
+            try
+            {
+                await _bookRepository.UpdateBookAsync(id, updatedBook);
+                return NoContent();
+            }
+            catch (ArgumentException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            try
+            {
+                await _bookRepository.DeleteBookAsync(id);
+                return NoContent();
+            }
+            catch (ArgumentException)
+            {
+                return NotFound();
+            }
         }
     }
 }
