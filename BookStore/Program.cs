a24cbe72e0b8a0baf03d97744f57bad611b030a6
Vipul <vipulupadhyay563@gmail.com>
2023-08-18 10:17:19 +0530

    Add Api CRUD Methods

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,4 +1,9 @@
 
+using BookStore.Data.Context;
+using BookStore.Data.Repository;
+using BookStore.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace BookStore
 {
     public class Program
@@ -8,6 +13,15 @@ namespace BookStore
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddDbContextPool<BookstoreDbContext>(options =>
+            {
+                // enable lazy loading
+                options.UseSqlServer(builder.Configuration.GetConnectionString("bookStoreDbEntities"));
+            });
+
+            builder.Services.AddScoped<IBookRepository, BookRepository>();
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
